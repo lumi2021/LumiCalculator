8ac3b8e156ea0c5aa044b92462bafdd7abf3a88a
lumi2021 <94462076+lumi2021@users.noreply.github.com>
2025-03-18 21:33:37 -0300

    fuck how should i resolve this expression

---
+++ Calculator/Number.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace Calculator;
+
+public struct Number
+{
+    private string _mantissa;
+    private int _exponent;
+
+    public Number(string mantissa, int exponent)
+    {
+        _mantissa = mantissa;
+        _exponent = exponent;
+    }
+    public Number(string number)
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < number.Length; i++)
+        {
+            if (number[i] == '.') _exponent = i;
+            else sb.Append(number[i]);
+        }
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new();
+
+        for (var i = 0; i < _mantissa.Length; i++)
+        {
+            sb.Append(_mantissa[i]);
+            if (i == _exponent) sb.Append('.');
+        }
+
+        return sb.ToString();
+    }
+}
--- Calculator/Screens/Standard.cs
+++ Calculator/Screens/Standard.cs
@@ -1,6 +1,8 @@
 ﻿using System.Numerics;
 using System.Text;
 using ImGuiNET;
+using Calculator;
+using System.Text.RegularExpressions;
 
 namespace Calculator.Screens;
 
@@ -9,10 +11,11 @@ public static class Standard
 
     private static List<string> expressionTokens = [];
     private static StringBuilder entryBuilder = new();
+    private static bool showingResult = false;
 
     public static void Update(double delta)
     {
-        
+
         ImGui.BeginChild("Screen", new Vector2(0, 100), ImGuiChildFlags.Border);
 
         string expression = string.Join(" ", expressionTokens);
@@ -41,11 +44,15 @@ public static class Standard
 
         ImGui.PushFont(Program.fontBig);
 
+
+        PushButtonStyles();
+
         if (ImGui.Button("%", new(buttonSizeX, buttonSizeY))) RegisterValue("%"); ImGui.SameLine();
         if (ImGui.Button("CE", new(buttonSizeX, buttonSizeY))) RegisterValue("CE"); ImGui.SameLine();
         if (ImGui.Button("C", new(buttonSizeX, buttonSizeY))) RegisterValue("C"); ImGui.SameLine();
 
         if (ImGui.Button("BS", new(buttonSizeX, buttonSizeY))) RegisterValue("BS");
+
 
         if (ImGui.Button("1/x", new(buttonSizeX, buttonSizeY))) RegisterValue("1/"); ImGui.SameLine();
         if (ImGui.Button("^", new(buttonSizeX, buttonSizeY))) RegisterValue("**"); ImGui.SameLine();
@@ -53,36 +60,60 @@ public static class Standard
 
         if (ImGui.Button("÷", new(buttonSizeX, buttonSizeY))) RegisterValue("/");
 
+        ImGui.PopStyleColor(3);
+
         if (ImGui.Button("7", new(buttonSizeX, buttonSizeY))) RegisterValue("7"); ImGui.SameLine();
         if (ImGui.Button("8", new(buttonSizeX, buttonSizeY))) RegisterValue("8"); ImGui.SameLine();
         if (ImGui.Button("9", new(buttonSizeX, buttonSizeY))) RegisterValue("9"); ImGui.SameLine();
 
+        PushButtonStyles();
         if (ImGui.Button("×", new(buttonSizeX, buttonSizeY))) RegisterValue("*");
+        ImGui.PopStyleColor(3);
 
         if (ImGui.Button("4", new(buttonSizeX, buttonSizeY))) RegisterValue("4"); ImGui.SameLine();
         if (ImGui.Button("5", new(buttonSizeX, buttonSizeY))) RegisterValue("5"); ImGui.SameLine();
         if (ImGui.Button("6", new(buttonSizeX, buttonSizeY))) RegisterValue("6"); ImGui.SameLine();
 
+        PushButtonStyles();
         if (ImGui.Button("-", new(buttonSizeX, buttonSizeY))) RegisterValue("-");
+        ImGui.PopStyleColor(3);
 
         if (ImGui.Button("1", new(buttonSizeX, buttonSizeY))) RegisterValue("1"); ImGui.SameLine();
         if (ImGui.Button("2", new(buttonSizeX, buttonSizeY))) RegisterValue("2"); ImGui.SameLine();
         if (ImGui.Button("3", new(buttonSizeX, buttonSizeY))) RegisterValue("3"); ImGui.SameLine();
 
+        PushButtonStyles();
         if (ImGui.Button("+", new(buttonSizeX, buttonSizeY))) RegisterValue("+");
+        ImGui.PopStyleColor(3);
 
         if (ImGui.Button("00", new(buttonSizeX, buttonSizeY))) RegisterValue("00"); ImGui.SameLine();
         if (ImGui.Button("0", new(buttonSizeX, buttonSizeY))) RegisterValue("0"); ImGui.SameLine();
         if (ImGui.Button(".", new(buttonSizeX, buttonSizeY))) RegisterValue("."); ImGui.SameLine();
 
+        PushButtonStyles();
         if (ImGui.Button("=", new(buttonSizeX, buttonSizeY))) RegisterValue("=");
+        ImGui.PopStyleColor(3);
 
         ImGui.PopFont();
 
     }
 
+    private static void PushButtonStyles()
+    {
+        ImGui.PushStyleColor(ImGuiCol.Button, Util.ColorVec2UInt(new(0.2f, 0.2f, 0.2f, 1f)));
+        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, Util.ColorVec2UInt(new(0.5f, 0.5f, 0.5f, 1f)));
+        ImGui.PushStyleColor(ImGuiCol.ButtonActive, Util.ColorVec2UInt(new(0.1f, 0.1f, 0.1f, 1f)));
+    }
+
+
     private static void RegisterValue(string c)
     {
+        if (showingResult)
+        {
+            ClearAll();
+            showingResult = false;
+        }
+
         switch (c)
         {
             case "CE":
@@ -90,8 +121,7 @@ public static class Standard
                 break;
 
             case "C":
-                expressionTokens.Clear();
-                entryBuilder.Clear();
+                ClearAll();
                 break;
 
             case "BS":
@@ -106,19 +136,19 @@ public static class Standard
 
             case "**":
                 TokenizeEntry();
-                AppendToken("^");
+                AppendToken("**");
                 break;
             case "//":
                 TokenizeEntry();
-                AppendToken("√");
+                AppendToken("//");
                 break;
 
             case "=":
-                // TODO: Evaluate expression
                 TokenizeEntry();
-                AppendToken(c);
+                ResolveExpression();
+                showingResult = true;
                 break;
-            
+
 
             default:
                 entryBuilder.Append(c);
@@ -132,4 +162,20 @@ public static class Standard
         expressionTokens.Add(entryBuilder.ToString().Trim());
         entryBuilder.Clear();
     }
+
+
+    private static void ClearAll()
+    {
+        expressionTokens.Clear();
+        entryBuilder.Clear();
+    }
+    private static void SyntaxError()
+    {
+        entryBuilder.AppendLine("= Syntax Error");
+    }
+    private static void ResolveExpression()
+    {
+        var tokens = new Queue<string>(expressionTokens);
+    }
+
 }
